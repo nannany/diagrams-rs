4bbc39936700a0606c252ac8a5a3320f005bc45b
minami.yoshihiko <nannany@hey.jp>
2022-05-22 13:51:35 +0900

    refactor. can compile

--- examples/try_dot.rs
+++ examples/try_dot.rs
@@ -4,36 +4,36 @@ fn main() {
     use std::fs::File;
     let mut f = File::create("example1.dot").unwrap();
 
-    let nodes = vec![
-        Box::new(Alb {
-            label: "aaa",
-            path: ("path1"),
-        }),
-        Box::new(Alb {
-            label: "bbb",
-            path: ("path2"),
-        }),
-        Box::new(Alb {
-            label: "ccc",
-            path: ("path3"),
-        }),
-        Box::new(Alb {
-            label: "ddd",
-            path: ("path4"),
-        }),
-    ];
+    let mut nodes = Vec::<Box<dyn Node>>::new();
+
+    nodes.push(Box::new(Alb {
+        label: "aaa",
+        path: ("path1"),
+    }));
+    nodes.push(Box::new(Alb {
+        label: "bbb",
+        path: ("path2"),
+    }));
+    nodes.push(Box::new(Alb {
+        label: "ccc",
+        path: ("path3"),
+    }));
+    nodes.push(Box::new(Alb {
+        label: "ddd",
+        path: ("path4"),
+    }));
     let edges = vec![(0, 1), (0, 2), (1, 3), (2, 3)];
     let graph = Graph { nodes, edges };
 
     graph.render_to(&mut f);
 }
 
-struct Alb {
-    label: &'static str,
-    path: &'static str,
+struct Alb<'a> {
+    label: &'a str,
+    path: &'a str,
 }
 
-impl Node for Alb {
+impl Node for Alb<'_> {
     fn label(&self) -> &str {
         &self.label
     }
